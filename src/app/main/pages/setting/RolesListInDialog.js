0a35226e5152e5655bd62ceb694fce5f104f85a5
M.Uzair <uzair.iu17@gmail.com>
2020-07-08 04:03:18 +0500

    donw with roles

---
+++ src/app/main/pages/setting/RolesListInDialog.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+const RolesListInDialog = function (props) {
+    const { roles, checkedRoles, classes, onInputChange } = props;
+    return (
+        <List dense className={classes.root}>
+            {roles.map(value => {
+                const labelId = `checkbox-list-secondary-label-${value.id}`;
+                return (
+                    <ListItem key={value.id} button>
+                        <ListItemText id={labelId} primary={`${value.name}`} />
+                        <ListItemSecondaryAction>
+                            <Checkbox
+                                name={'roles'}
+                                value={value.id}
+                                edge="end"
+                                onChange={onInputChange}
+                                checked={checkedRoles.includes(value.id) && true}
+                                inputProps={{ 'aria-labelledby': labelId }}
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                );
+            })}
+        </List>
+    )
+};
+
+export default RolesListInDialog;
