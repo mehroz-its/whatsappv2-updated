e6d1a9b2de49d970da3f7b177838d2d64108cf44
khanmuhammadarshad <arshad.khan@intellexalsolutions.com>
2020-10-19 03:48:20 -0700

    routes error

--- src/app/main/pages/Reports/ReportsAppConfig.js
+++ src/app/main/pages/Reports/ReportsAppConfig.js
@@ -9,7 +9,7 @@ const ReportsAppConfig = {
 	routes: [
 		{
 			path: '/report/chat',
-			component: React.lazy(() => import('./Chat/ChatApp'))
+			component: React.lazy(() => import('./chat/ChatApp'))
 		},
 		{
 			path: '/report/agent-report',
