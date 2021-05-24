ba949cebd06f01e2e96faaa1bfb8d394e7e893f7
UzairSammad <uzair.iu17@gmail.com>
2021-05-24 17:47:06 +0500

    done with changes

--- src/app/main/pages/adminDashboard/widgets/AgentWidgets.js
+++ src/app/main/pages/adminDashboard/widgets/AgentWidgets.js
@@ -7,7 +7,7 @@ function AgentWidget({ title, agents }) {
 		<>
 			<Paper className="w-full rounded-8 shadow-none border-1">
 				<div className="text-center pt-24 ">
-					<Typography className="text-24 px-12 leading-none text-red"> {agents}</Typography>
+					<Typography className="px-12 leading-none text-red" style={{ fontSize: '20px' }}> {agents}</Typography>
 				</div>
 				<div className="flex items-center justify-center mt-20  h-40 border-t-1">
 					<Typography className="px-10" color="textSecondary">
